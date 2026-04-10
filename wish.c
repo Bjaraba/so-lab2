e7aa9f0ee1eedc57205b6eefbde93ff3a0a5fcd1
brayan <b.jaraba@udea.edu.co>
2026-04-10 13:32:50 -0500

    feat: handel inputs to get the command and a list of the arguments

--- wish.c
+++ wish.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 char *exit_str = "exit";
 char *path = "/bin";
 
+void print_arr(char **arr);
 void handle_input(char *input);
+void handle_extern_commands(char *command, char **args);
+void extern_command(char *command, char **args);
+
 int main(int argc, char const *argv[])
 {
   while (1)
@@ -35,24 +40,99 @@ int main(int argc, char const *argv[])
 
 void handle_input(char *input)
 {
+  if (strlen(input) == 0)
+  {
+    return; // skip void lines
+  }
+  char *input_copy = malloc(strlen(input) + 1);
+  strcpy(input_copy, input); // Copy input
+
   char path[256];
   strcpy(path, "/bin/");
-  char *token;
-  token = strsep(&input, " ");
 
-  if (token == NULL)
+  char *command = NULL;
+  char **args = NULL;
+  char *item;
+  int i = 0;
+
+  while ((item = strsep(&input_copy, " ")) != NULL)
+  {
+    if (strlen(item) == 0)
+    {
+      continue; // avoid void words
+    }
+    if (i == 0)
+    {
+      command = malloc(strlen(item) + 1);
+      strcpy(command, item); // first word is the command
+      i++;
+      continue;
+    }
+    args = realloc(args, (i + 1) * sizeof(char *));
+    args[i - 1] = malloc(strlen(item) + 1);
+    strcpy(args[i - 1], item);
+    args[i] = NULL;
+    i++;
+  }
+
+  if (command == NULL)
   {
     printf("comando no disponible");
   }
-
-  strcat(path, token);
-  int available = access(path, X_OK);
-  if (available == 0)
+  else
   {
-    printf("el ejecutable: '%s' esta dispolible\n", path);
+    printf("commando: %s\n", command);
+    strcat(path, command);
+    int available = access(path, X_OK);
+    if (available == -1)
+    {
+      printf("el comando no esta disponible\n");
+    }
+    else
+    {
+      handle_extern_commands(command, args);
+    }
+  }
+
+  // Free memory
+  if (command != NULL)
+  {
+    free(command);
+  }
+  if (args != NULL)
+  {
+    for (int j = 0; args[j] != NULL; j++)
+    {
+      free(args[j]);
+    }
+    free(args);
+  }
+  free(input_copy);
+}
+
+void handle_extern_commands(char *command, char **args)
+{
+  int pid = fork();
+  if (pid < 0)
+  {
+    printf("ha ocurrido un error\n");
+  }
+  else if (pid == 0)
+  {
   }
   else
   {
-    printf("comando no disponible\n");
+    wait(NULL);
+  }
+}
+
+void print_arr(char **arr)
+{
+  int count = 0;
+  char *item;
+  while ((item = arr[count]) != NULL)
+  {
+    printf("item %d: %s\n", count, item);
+    count++;
   }
 }
